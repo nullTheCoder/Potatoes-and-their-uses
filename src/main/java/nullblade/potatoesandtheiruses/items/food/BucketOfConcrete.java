ec14f9c21d50f6645f0b1cd66637553b6241b658
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-09 09:01:29 +0300

    Wrong model

--- src/main/java/nullblade/potatoesandtheiruses/items/food/BucketOfConcrete.java
+++ src/main/java/nullblade/potatoesandtheiruses/items/food/BucketOfConcrete.java
@@ -2,6 +2,7 @@ package nullblade.potatoesandtheiruses.items.food;
 
 import nullblade.potatoesandtheiruses.PotatoMain;
 import org.waveapi.api.content.items.WaveItem;
+import org.waveapi.api.content.items.models.SimpleItemModel;
 import org.waveapi.api.content.items.models.SimpleToolModel;
 import org.waveapi.api.content.items.recipes.WaveShapedRecipe;
 import org.waveapi.api.file.texture.Texture;
@@ -9,7 +10,7 @@ import org.waveapi.api.file.texture.Texture;
 public class BucketOfConcrete extends WaveItem {
     public BucketOfConcrete() {
         super("bucket_of_concrete", PotatoMain.instance);
-        setModel(new SimpleToolModel(new Texture("potato_uses/items/food/bucket_of_concrete.png")));
+        setModel(new SimpleItemModel(new Texture("potato_uses/items/food/bucket_of_concrete.png")));
         setTab(PotatoMain.tab);
         makeFood(4, 0.5f);
 
--- src/main/java/nullblade/potatoesandtheiruses/items/food/EdibleBucket.java
+++ src/main/java/nullblade/potatoesandtheiruses/items/food/EdibleBucket.java
@@ -2,6 +2,7 @@ package nullblade.potatoesandtheiruses.items.food;
 
 import nullblade.potatoesandtheiruses.PotatoMain;
 import org.waveapi.api.content.items.WaveItem;
+import org.waveapi.api.content.items.models.SimpleItemModel;
 import org.waveapi.api.content.items.models.SimpleToolModel;
 import org.waveapi.api.content.items.recipes.WaveShapedRecipe;
 import org.waveapi.api.file.texture.Texture;
@@ -9,7 +10,7 @@ import org.waveapi.api.file.texture.Texture;
 public class EdibleBucket extends WaveItem {
     public EdibleBucket() {
         super("edible_bucket", PotatoMain.instance);
-        setModel(new SimpleToolModel(new Texture("potato_uses/items/food/edible_bucket.png")));
+        setModel(new SimpleItemModel(new Texture("potato_uses/items/food/edible_bucket.png")));
         setTab(PotatoMain.tab);
         makeFood(2, 0.5f);
 
